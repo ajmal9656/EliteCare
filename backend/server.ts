6f940888ac895c54b9d5888eb3c114882b300ad3
ajmal <ajmalbinzali@gmail.com>
2024-11-28 20:39:34 +0530

    "coookie config changed"

--- backend/server.ts
+++ backend/server.ts
@@ -33,7 +33,7 @@ app.use(express.json());
 const corsOptions = {
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
-  credentials: true  // Allow credentials (cookies, authorization headers, etc.)
+  credentials: true  // Allow credentials (cookies, authorization headers, etc.),
 };
 
 
