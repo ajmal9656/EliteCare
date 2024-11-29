ead8fa17183a09a440c54263ca2bdf6365cad314
ajmal <ajmalbinzali@gmail.com>
2024-11-29 18:11:38 +0530

    "doctor profile design change"

--- frontend/src/pages/userPages/DoctorProfileLayout.tsx
+++ frontend/src/pages/userPages/DoctorProfileLayout.tsx
@@ -14,30 +14,30 @@ function DoctorProfileLayout() {
   console.log("Appointment Data:", appointmentData);
 
   return (
-    <div className='w-[100%]  flex flex-col bg-gray-100'>
-      {/* Doctor's Profile Image */}
-      <div className='w-[100%] h-[300px] bg-gradient-to-r from-[#D2EFEA] to-[#ADE9DC] flex place-content-center'>
-        <div className='w-[13%] h-[200px] mt-16 relative '>
-          {/* Render the doctor's image only if doctor data is available */}
-          {doctor ? (
-            <img
-              src={doctor.signedImageUrl}
-              alt={doctor.name}
-              className='w-full h-full object-cover z-10 mt-28 rounded-md'
-            />
-          ) : (
-            <p>No doctor data available</p>
-          )}
-        </div>
-      </div>
-
-      {/* Pass doctor and appointmentData (if available) through Outlet context */}
-      {appointmentData ? (
-        <Outlet context={{ doctor, appointmentData }} />
+    <div className='w-full flex flex-col bg-gray-100'>
+  {/* Doctor's Profile Image */}
+  <div className='w-full h-[300px] bg-gradient-to-r from-[#D2EFEA] to-[#ADE9DC] flex justify-center'>
+    <div className='w-[13%] h-[200px] mt-16 relative sm:w-[20%] md:w-[10%] lg:w-[13%]'>
+      {doctor ? (
+        <img
+          src={doctor.signedImageUrl}
+          alt={doctor.name}
+          className='w-full h-full object-cover z-10 mt-28 rounded-md'
+        />
       ) : (
-        <Outlet context={{ doctor }} />
+        <p>No doctor data available</p>
       )}
     </div>
+  </div>
+
+  {/* Pass doctor and appointmentData (if available) through Outlet context */}
+  {appointmentData ? (
+    <Outlet context={{ doctor, appointmentData }} />
+  ) : (
+    <Outlet context={{ doctor }} />
+  )}
+</div>
+
   );
 }
 
